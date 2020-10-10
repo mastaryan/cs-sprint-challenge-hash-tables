d6ed858504c15440f58dba12f2eb3bcc06895e93
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-10-10 16:27:21 -0400

    Ex1 completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+
+    for i in range(length):
+        current = weights[i]
+        remaining = limit - current
+
+        if remaining in cache:
+            if cache[remaining] > i:
+                return[cache[remaining], i]
+            else:
+                return [i, cache[remaining]]
+        else:
+            cache[current] = i
 
     return None
