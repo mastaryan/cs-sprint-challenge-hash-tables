7dbb98e7a42aefedccd6e2729f6bb82c305c5221
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-10-10 17:06:56 -0400

    ex3 completed

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,11 +1,24 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    result = []
+    
+    for i in range(len(arrays[0])):
+        cache[arrays[0][i]] = 1
+        
+    for arr in range(1, len(arrays)):
+        array_num = arrays[arr]
+        
+        for i in range(len(array_num)):
+            if array_num[i] in cache:
+                cache[array_num[i]] += 1
+    
+    filtered_cache = [(key, cache[key]) for key in cache if cache[key] == len(arrays)]
+    
+    for key, value in filtered_cache:
+        result.append(key)
+        
 
     return result
-
 
 if __name__ == "__main__":
     arrays = []
